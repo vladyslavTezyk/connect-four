81ae17197c7f8c889373915925e0c713b7ffbe49
Vladyslav Tezyk <vlad.tezyk.pro@gmail.com>
2026-04-14 08:19:03 +0200

    feat: add board display

---
+++ apps/server/src/core/displayBoard.ts
@@ -0,0 +1,17 @@
+import { GameBoard } from "@connect-four/shared";
+
+enum Cell {
+  Empty = 0,
+  Player1 = 1,
+  Player2 = 2,
+}
+
+const cellRepresentation = (cell: Cell) => {
+  if (cell === Cell.Player1) return "🔴";
+  if (cell === Cell.Player2) return "🟡";
+  return "⚫";
+};
+
+export function displayBoard(gameBoard: GameBoard) {
+  console.log(gameBoard.grid.map((row) => row.map(cellRepresentation).join(" ")).join("\n"));
+}
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,14 +1,21 @@
 import type { GameBoard } from "@connect-four/shared";
-import { initBoard } from './core/initBoard';
-import { selectColumn } from './core/selectColumn';
+import { initBoard } from "./core/initBoard";
+import { selectColumn } from "./core/selectColumn";
+import { placeToken } from "./core/placeToken";
+import { displayBoard } from "./core/displayBoard";
 
-async function runConnectFour(){
-    const gameBoard: GameBoard = initBoard();
-    console.log(gameBoard);
-
-    const column = await selectColumn(gameBoard.grid);
-
+async function runConnectFour() {
+  const gameBoard: GameBoard = initBoard();
+  
+  while (gameBoard.win === null) {
+    displayBoard(gameBoard);
+    const column = await selectColumn(gameBoard);
     console.log(`Player ${gameBoard.currentPlayer} chose column ${column}`);
+    placeToken(gameBoard, column);
+    if (gameBoard.currentRound >= 7) {
+      console.log("Checking for win condition...");
+    }
+  }
 }
 
 runConnectFour();
