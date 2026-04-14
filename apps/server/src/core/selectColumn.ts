245384393d4b909663d7597cb684eff82dda8c8c
Vladyslav Tezyk <vlad.tezyk.pro@gmail.com>
2026-04-14 08:18:04 +0200

    fix(selectColumn): add missing edge cases

--- apps/server/src/core/selectColumn.ts
+++ apps/server/src/core/selectColumn.ts
@@ -1,17 +1,28 @@
-import * as readline from 'node:readline/promises';
-import { stdin as input, stdout as output } from 'node:process';
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+import { GameBoard } from "@connect-four/shared";
 
-export async function selectColumn(gameBoard: number[][]): Promise<number> {
-    const rl = readline.createInterface({ input, output });
-    const column = await rl.question('What column would you like to place your token in? ');
-    
-    const maxColumn = gameBoard[0].length;
-    if(parseInt(column) < 0 || parseInt(column) > maxColumn){
-        console.log('Invalid column. Please choose a column between 1 and 7.');
-        return selectColumn(gameBoard);
-    }
+export async function selectColumn(gameBoard: GameBoard): Promise<number> {
+  const rl = readline.createInterface({ input, output });
+  const column = parseInt(
+    await rl.question(`Round ${gameBoard.currentRound} \n ${gameBoard.currentPlayer === 1 ? "Player 1" : "Player 2"}: 
+    What column would you like to place your token in? `)
+  );
+  const maxColumn = gameBoard.grid[0].length;
 
-    rl.close();
+  // Check if the column is valid
+  if (column < 0 || column > maxColumn) {
+    console.log("Invalid column. Please choose a column between 1 and 7.");
+    return selectColumn(gameBoard);
+  }
 
-    return parseInt(column);
+  // Check if the column is full
+  if (gameBoard.grid[0][column - 1] !== 0) {
+    console.log("Column is full. Please choose a different column.");
+    return selectColumn(gameBoard);
+  }
+
+  rl.close();
+
+  return column;
 }
